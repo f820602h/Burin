663b07c4bb050db3730566e3990e2fef29735c4a
max.lee <f820602h@gmail.com>
2022-11-17 14:31:20 +0800

    [adjust] service worker post message 溝通的 typing

--- src/service/sw.ts
+++ src/service/sw.ts
@@ -8,9 +8,14 @@ import {
   unsubscribeTaskNotification,
 } from "./modules/taskNotification";
 
-const methods: Record<string, (data: any) => void> = {
-  subscribeTaskNotification,
-  unsubscribeTaskNotification,
+enum ServiceWorkerMethods {
+  SUBSCRIBE = "subscribeTaskNotification",
+  UNSUBSCRIBE = "unsubscribeTaskNotification",
+}
+
+export const serviceWorkerMethods = {
+  [ServiceWorkerMethods.SUBSCRIBE]: subscribeTaskNotification,
+  [ServiceWorkerMethods.UNSUBSCRIBE]: unsubscribeTaskNotification,
 };
 
 self.addEventListener("install", function (event) {
@@ -23,37 +28,8 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("message", function (event) {
   if (event.origin !== self.location.origin) return;
-  methods[event.data.method](event.data.data);
-});
-
-self.addEventListener("push", function (event) {
-  event.waitUntil(
-    // Retrieve a list of the clients of this service worker.
-    self.clients.matchAll({ type: "window" }).then(function (clientList) {
-      // Check if there's at least one focused client.
-      const focused = clientList.some(function (client) {
-        return client.focused;
-      });
-
-      let notificationMessage;
-      if (focused) {
-        notificationMessage = "You're still here, thanks!";
-      } else if (clientList.length > 0) {
-        notificationMessage =
-          "You haven't closed the page, " + "click here to focus it!";
-      } else {
-        notificationMessage =
-          "You have closed the page, " + "click here to re-open it!";
-      }
-
-      // Show a notification with title 'ServiceWorker Cookbook' and body depending
-      // on the state of the clients of the service worker (three different bodies:
-      // 1, the page is focused; 2, the page is still open but unfocused; 3, the page
-      // is closed).
-      return self.registration.showNotification("ServiceWorker Cookbook", {
-        body: notificationMessage,
-      });
-    })
+  serviceWorkerMethods[event.data.method as ServiceWorkerMethods](
+    event.data.data
   );
 });
 
