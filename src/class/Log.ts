ca6f5b246313efd70f52432ffcf4d45b2ba035a3
max.lee <f820602h@gmail.com>
2023-03-14 10:25:06 +0800

    [adjust] log 格式調整/統計資料調整

--- src/class/Log.ts
+++ src/class/Log.ts
@@ -8,11 +8,15 @@ export interface LogId {
 }
 
 export interface LogInfo extends LogId {
+  categoryId: TaskCategory["id"];
   categoryName: TaskCategory["name"];
+  categoryUpdateTimestamp: TaskCategory["updateTimestamp"];
   startColor: TaskCategory["startColor"];
   endColor: TaskCategory["endColor"];
+  taskId: Task["id"];
   taskTitle: Task["title"];
   taskAdditionContent: Task["additionContent"];
+  taskUpdateTimestamp: Task["updateTimestamp"];
   startTimestamp: Task["lastStartTimestamp"];
   endTimestamp: Task["lastEndTimestamp"];
 }
@@ -23,22 +27,30 @@ export interface LogCompleteInfo extends LogInfo {
 
 export class Log implements LogCompleteInfo {
   id: LogCompleteInfo["id"];
+  categoryId: LogCompleteInfo["categoryId"];
   categoryName: LogCompleteInfo["categoryName"];
+  categoryUpdateTimestamp: LogCompleteInfo["categoryUpdateTimestamp"];
   startColor: LogCompleteInfo["startColor"];
   endColor: LogCompleteInfo["endColor"];
+  taskId: LogCompleteInfo["taskId"];
   taskTitle: LogCompleteInfo["taskTitle"];
   taskAdditionContent: LogCompleteInfo["taskAdditionContent"];
+  taskUpdateTimestamp: LogCompleteInfo["taskUpdateTimestamp"];
   startTimestamp: LogCompleteInfo["startTimestamp"];
   endTimestamp: LogCompleteInfo["endTimestamp"];
   createTimestamp: LogCompleteInfo["createTimestamp"];
 
   constructor(arg: LogCompleteInfo) {
     this.id = arg.id;
+    this.categoryId = arg.categoryId;
     this.categoryName = arg.categoryName;
+    this.categoryUpdateTimestamp = arg.categoryUpdateTimestamp;
     this.startColor = arg.startColor;
     this.endColor = arg.endColor;
+    this.taskId = arg.taskId;
     this.taskTitle = arg.taskTitle;
     this.taskAdditionContent = arg.taskAdditionContent;
+    this.taskUpdateTimestamp = arg.taskUpdateTimestamp;
     this.startTimestamp = arg.startTimestamp;
     this.endTimestamp = arg.endTimestamp;
     this.createTimestamp = arg.createTimestamp;
@@ -60,11 +72,15 @@ export class Log implements LogCompleteInfo {
       throw Error("task and task category cannot be associated");
     }
     return {
+      categoryId: taskCategory.id,
       categoryName: taskCategory.name,
+      categoryUpdateTimestamp: taskCategory.updateTimestamp,
       startColor: taskCategory.startColor,
       endColor: taskCategory.endColor,
+      taskId: task.id,
       taskTitle: task.title,
       taskAdditionContent: task.additionContent,
+      taskUpdateTimestamp: task.updateTimestamp,
       startTimestamp: task.lastStartTimestamp,
       endTimestamp: task.lastEndTimestamp,
     };
