1ccf9e44ffb704adb8e36769b025ec20711fd62b
max.lee <f820602h@gmail.com>
2023-04-21 17:02:32 +0800

    [adjust] 調整 service worker

---
+++ src/service/modules/currentLogNotification.ts
@@ -0,0 +1,51 @@
+import type { CurrentLog } from "@/class/Log";
+
+declare let self: ServiceWorkerGlobalScope;
+
+type CurrentLogNotificationData = {
+  title: CurrentLog["title"];
+  startTimestamp: CurrentLog["startTimestamp"];
+  pauseDuration: CurrentLog["pauseDuration"];
+  intervalMinutes: number;
+};
+
+let afterTimes = 0;
+let intervalId = 0;
+
+function calcAfterTimes(data: CurrentLogNotificationData): number {
+  const now = new Date().getTime();
+  return Math.floor(
+    (now - data.startTimestamp - data.pauseDuration) /
+      (data.intervalMinutes * 1000)
+  );
+}
+
+function initAfterTimes(data: CurrentLogNotificationData): void {
+  afterTimes = calcAfterTimes(data);
+}
+
+function sendNotification(data: CurrentLogNotificationData): void {
+  intervalId = self.setInterval(() => {
+    const newAfterTimes = calcAfterTimes(data);
+    if (afterTimes < newAfterTimes) {
+      afterTimes = newAfterTimes;
+      self.registration.showNotification("Burin", {
+        body: `${data.title} 已經進行 ${
+          data.intervalMinutes * afterTimes
+        } 分鐘囉！`,
+      });
+    }
+  }, 500);
+}
+
+export function subscribeCurrentLogNotification(
+  data: CurrentLogNotificationData
+): void {
+  initAfterTimes(data);
+  sendNotification(data);
+}
+
+export function unsubscribeCurrentLogNotification(): void {
+  afterTimes = 0;
+  self.clearInterval(intervalId);
+}
