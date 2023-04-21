861906a949f2b9081a2af7ddaa1e51b1fb3ab404
max.lee <f820602h@gmail.com>
2023-04-21 17:10:07 +0800

    [adjust] 日期選單資料移至元件中

--- src/stores/stamp.ts
+++ src/stores/stamp.ts
@@ -1,7 +1,5 @@
 import { defineStore } from "pinia";
 import { axiosStampGetList } from "@/api/stamp/index";
-import { monthNameGetter } from "@/helper/monthNameGetter";
-import type { MenuItemToggle } from "@/components/global/sidebar/types";
 
 export type StampState = {
   stamps: number[];
@@ -12,40 +10,6 @@ export const useStampStore = defineStore({
   state: (): StampState => ({
     stamps: [],
   }),
-  getters: {
-    _getYearMonthDateMenuItems(): MenuItemToggle[] {
-      return this.stamps.reduce<MenuItemToggle[]>((acc, timestamp) => {
-        const year: string = new Date(timestamp).getFullYear().toString();
-        const month: string = monthNameGetter(new Date(timestamp).getMonth());
-        const date: string = new Date(timestamp).getDate().toString();
-
-        const yearItem = acc.find((item) => item.name === year);
-        if (!yearItem) {
-          acc.push({
-            name: year,
-            children: [{ name: month, children: [{ name: date }] }],
-          });
-          return acc;
-        }
-
-        const monthItem = yearItem?.children.find(
-          (item) => item.name === month
-        );
-        if (!monthItem || !("children" in monthItem)) {
-          yearItem.children.push({ name: month, children: [{ name: date }] });
-          return acc;
-        }
-
-        const dateItem = monthItem?.children.find((item) => item.name === date);
-        if (!dateItem) {
-          monthItem.children.push({ name: date });
-          return acc;
-        }
-
-        return acc;
-      }, []);
-    },
-  },
   actions: {
     async _actFetchStamps(): Promise<void> {
       const date = await axiosStampGetList();
